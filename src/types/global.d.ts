04449df3924ea1bfa13647eac0dbf26fde4b1770
miguelogf <miguel.gallardof@icloud.com>
2026-03-02 06:36:21 -0500

    fix: update toast

---
+++ src/types/global.d.ts
@@ -0,0 +1,10 @@
+export {}
+
+declare global {
+  interface Window {
+    showToast: (options: {
+      message: string
+      variant?: "success" | "warning" | "danger" | "info"
+    }) => void
+  }
+}
