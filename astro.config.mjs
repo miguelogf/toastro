92449b1dafcc5687bd3b006f05d0dd29dde239c9
miguelogf <miguel.gallardof@icloud.com>
2026-03-10 04:02:08 -0500

    fix: update astrojs/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  output: "static",
   adapter: vercel(),
 });
