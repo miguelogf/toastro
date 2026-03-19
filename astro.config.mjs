d0ddf8c33aabb08487ddbbd7b82cc75df2432fb0
Miguel Gallardo <miguel@Mac-mini-de-Miguel.local>
2026-03-18 22:27:29 -0500

    chore: update components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,17 @@
 import { defineConfig } from "astro/config";
+import path from "path";
 
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+        "@ui": path.resolve("./src/components/ui"),
+        "@sections": path.resolve("./src/components/sections"),
+        "@data": path.resolve("./data"),
+        "@layouts": path.resolve("./src/layouts"),
+        "@utils": path.resolve("./src/utils"),
+      },
+    },
+  },
+});
